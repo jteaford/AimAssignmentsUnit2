0de363dcafe2c7b2938edd87a80ab7f0439de91c
jteaford <jessteaford@gmail.com>
2020-06-18 09:28:14 -0500

    Initial Commit

---
+++ src/Assignment_2_2.java
@@ -0,0 +1,5 @@
+public class Assignment_2_2 {
+    public static void main (String [] args) {
+        
+    }
+}
---
+++ src/Assignment_2_3.java
@@ -0,0 +1,5 @@
+public class Assignment_2_3 {
+    public static void main (String [] args) {
+        
+    }
+}
