8ff9341995ce84e01b7314e589b9c750b14b6ada
jteaford <jessteaford@gmail.com>
2020-06-18 15:44:00 -0500

    Initial Commit

--- src/Assignment_2_3.java
+++ src/Assignment_2_3.java
@@ -5,17 +5,18 @@ public class Assignment_2_3 {
         System.out.println("This program calculates pounds per kilogram in a table.");
 
         //Print table header
-        System.out.println("\tkilograms     pounds");
+        System.out.println("\n\tkilograms     pounds");
         System.out.println("\t---------     ------");
 
         
 
-        for(int i = 1; i < 15; i++){
-        int kilograms = 1;
+        for(int i = 1; i <= 15; i += 2){
+        int kilograms = i;
         double pounds = kilograms * 2.2;
-        System.out.format("%9d\t     %4.1f", kilograms, pounds);
+        System.out.format("\t%-9d     %-4.1f\n", kilograms, pounds);
         }
 
+        System.out.println("\nGoodbye ...");
         System.out.println("\n");
     }
 }
